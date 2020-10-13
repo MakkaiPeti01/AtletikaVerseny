6763d17445f2ddc8397e7d31c01f09a0de9df367
MakkaiPeti01 <PetyaPower@gmail.com>
2020-10-13 08:54:02 +0200

    kesz, de megse

---
+++ AtletikaiVerseny/Atleta.cs
@@ -0,0 +1,24 @@
+﻿namespace AtletikaiVerseny
+{
+    internal class Atleta
+    {
+        public string Rajtszam { get; private set; }
+        public string VezNev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesulet { get; private set; }
+        public int Ugras { get; private set; }
+        public Atleta(string sor)
+        {
+            string[] adat = sor.Split(';');
+            Rajtszam = adat[0];
+            VezNev = adat[1];//Ez a kettő egy
+            KerNev = adat[2];
+            Egyesulet = adat[3];
+            Ugras = int.Parse(adat[4]);
+        }
+        public string Nev
+        {
+            get { return VezNev + " " + KerNev; }
+        }
+    }
+}
--- AtletikaiVerseny/Program.cs
+++ AtletikaiVerseny/Program.cs
@@ -3,13 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AtletikaiVerseny
 {
     class Program
     {
+        static List<Atleta> lista = new List<Atleta>();
+        static Dictionary<string, int> csapatok = new Dictionary<string, int>();
+        static void Beolvas()
+        {
+            StreamReader olvas = new StreamReader("tavol.cs");
+            while (!olvas.EndOfStream)
+            {
+                lista.Add(new Atleta(olvas.ReadLine()));
+            }
+            olvas.Close();
+            foreach (var i in lista)
+            {
+                Console.WriteLine(i.Egyesulet);
+            }
+        }
+        static void Masodik()
+        {
+            foreach (var i in lista)
+            {
+                Console.WriteLine("2. feladat: Nevek és ugrások\n" +
+                    "{0} {1} {2}", i.VezNev + " " + i.KerNev, i.Ugras);
+            }
+        }
+        static void Harmadik()
+        {
+            foreach (var i in lista)
+            {
+                if (!csapatok.ContainsKey(i.Egyesulet))
+                {
+                    csapatok.Add(i.Egyesulet, 0);
+                }
+            }
+            foreach (var i in csapatok)
+            {
+                Console.WriteLine("3. feladat: Egyesületek:\n" +
+                    "{0}", i.Key);
+            }
+        }
+        static void Negyedik()
+        {
+            int max = 0;
+            string neve = "";
+            foreach (var i in lista)
+            {
+                if (i.Ugras > max)
+                {
+                    max = i.Ugras;
+                    neve = i.Nev;
+                }
+            }
+            Console.WriteLine("4. feladat: Legnagyobb ugrás: \n" +
+                "{0}: {1} cm", neve, max);
+        }
+        static void Otodik()
+        {
+            double atlag;
+            int sum = 0;
+            int db = 0;
+            foreach (var i in lista)
+            {
+                sum = sum + i.Ugras;
+            }
+            atlag = sum / lista.Count;
+            foreach (var i in lista)
+            {
+                if (i.Ugras < atlag)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine("5. feladat: Átlag alatt lévő ugrások száma: {0}", db);
+        }
+        static void Hatodik()
+        {
+            StreamWriter iro = new StreamWriter("versenyzok.txt");
+            foreach (var i in lista)
+            {
+                iro.WriteLine("{0};{1}",i.Rajtszam,i.Nev);
+            }
+            iro.Close();
+        }
         static void Main(string[] args)
         {
+            var h = new Atleta("623;Ug Imre;Kalocsai Rozmarok;640");
+            Beolvas();
+            Masodik();
+            Harmadik();
+            Negyedik();
+            Otodik();
+            Hatodik();
+            Console.ReadKey();
         }
     }
 }
